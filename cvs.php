28dcf5663069be7f1b2ed200e3fad40ee9502b79
Oleg Nesterov <NZOlegok@gmail.com>
2017-06-12 15:59:46 +1200

    added support of formulas

--- cvs.php
+++ cvs.php
@@ -6,14 +6,16 @@ use MathParser\Interpreting\Evaluator;
 
 $parser = new StdMathParser();
 $evaluator = new Evaluator();
-
+$variables = [];
 $rows = $_POST['rows'];
 for($i=1; $i<=$rows; $i++){
+    global $variables;
     $AST = $parser->parse($_POST[$i]);
-    $evaluator->setVariables([ 'x' => rand($_POST["{$i}min"],$_POST["{$i}max"])]);
+    $evaluator->setVariables(array_merge([ 'x' => rand($_POST["{$i}min"],$_POST["{$i}max"])], $variables));
     $value = $AST->accept($evaluator);
-
-    echo "value=$value ";
+    $variables["z{$i}"] = $value;
+    var_dump($variables);
+    echo "value=$value";
 }
 
 
--- generate.php
+++ generate.php
@@ -1,29 +1,30 @@
+<?php include "header.php"; ?>
 <div class="container-fluid">
 <form method="post" action="cvs.php">
-<?php
-    $rows = $_GET['rows'];
-    echo "<div class='row'><span class='col-sm-2'>Formula</span><span class='col-sm-2'>Min</span><span class='col-sm-2'>Max</span><span class='col-sm-2'>Scale of random</span><span class='col-sm-2'>Type</span></div>";
-    echo '</br>';
-    for ($i = 1; $i<=$rows; $i++){
-        echo "<div class='row'>";
-        echo "z{$i}:";
-        echo "<input type='text' name='$i' class='col-sm-2'>";
-        echo "<input type='text' name='{$i}min' class='col-sm-2'>";
-        echo "<input type='text' name='{$i}max' class='col-sm-2'> ";
-        echo "<input type='text' name='{$i}rand' class='col-sm-2'>";
-        echo "<select class='col-sm-2'>
-              <option value='int'>Integer</option>
-              <option value='name'>Name</option>
-              <option value='bin'>Yes and No</option>
-              <option value='dec'>Decimal</option>
-              </select>";
-        echo "<input type='hidden' name='rows' value='{$rows}'>";
-        echo '</div>';
-    };
-    echo '<input type="submit"  class="submit">';
+    <div class="row">
+        <div class="col-sm-3 offset-sm-1"> Formula</div>
+        <div class="col-sm-2"> Min</div>
+        <div class="col-sm-2"> Max</div>
+
+    </div>
+<?php $rows = $_GET['rows'];
+for ($i=1; $i<=$rows; $i++):
 ?>
+    <div class="row">
+        <div class="col-sm-1">Z<?=$i?></div>
+        <input type="text" class="col-sm-3"  name="<?=$i ?>">
+        <input type="text" class="col-sm-2" name="<?=$i ?>min">
+        <input type="text" class="col-sm-2" name="<?=$i ?>max">
+
+    </div>
+
+</br>
+<?php endfor; ?>
+    <div class="row">
+        <input type="hidden" value="<?=$rows?>" name="rows">
+        <input type="submit" value="submit" class="col-sm-2 offset-sm-3">
+    </div>
 </form>
 </div>
-
 <?php include "footer.php"; ?>
 
